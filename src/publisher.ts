d817ade796b153f6627bf10dddcb1d8ca66290fa
GreatEki <ekeneidiagbor@gmail.com>
2023-04-03 15:56:26 +0100

    feat: created base publisher and ticket created publisher classes

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/publisher/TicketCreatedPublisher";
 
 console.clear();
 
@@ -9,13 +10,11 @@ const stan = nats.connect("ticketing", "abc", {
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
 
-  const message = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+
+  publisher.publish({
     id: "123",
     title: "concert",
-    price: 20,
-  });
-
-  stan.publish("ticket:created", message, () => {
-    console.log("Event published");
+    price: 50,
   });
 });
